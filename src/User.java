4bfe4f64a3268fdb8fc990c53319a523fbf61357
anthony-Soto <asoto30@calpoly.edu>
2026-04-29 14:23:33 -0700

    finished user and login, let me know if you guys think I should change anything

--- src/User.java
+++ src/User.java
@@ -1,8 +1,12 @@
+package src;
 import java.util.*;
 
 public class User {
+    // User class to represent a user in the system
     private String name;
     private String email;
+    private LinkedList<Project> projects = new LinkedList<>();    
+
 
     public User(String name, String email) {
         this.name = name;
@@ -17,17 +21,35 @@ public class User {
         return email;
     }
 
-    LinkedList<Project> projects = new LinkedList<>();    
-
-    public void addProject(Project project) {
+    // Method to add a project to the user's list of projects
+    public void addProjectToUser(Project project) {
+        System.out.println("Adding project: " + project.getProjectName() + " to user: " + this.name);
         projects.add(project);
     }
 
+    // Method to get the list of projects associated with the user
     public LinkedList<Project> getProjects() {
         return projects;
     }
-    public void removeProject(Project project) {
+
+    // Method to remove a project from the user's list of projects
+    public void removeUserProject(Project project) {
+        System.out.println("Removing project: " + project.getProjectName() + " from user: " + this.name);
         projects.remove(project);
     }
 
+    // Method to display user's projects
+    public void displayUserProjects() {
+        System.out.println("Projects for user: " + this.name);
+        for (Project project : projects) {
+            System.out.println("- " + project.getProjectName());
+        }
+    }
+
+
+    // Override toString for better user representation
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', email='" + email + "'}";
+    }
 }
