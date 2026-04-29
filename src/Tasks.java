c1effd1da75750f948815a7319ebbc95ddebba51
itorriani <itorrian@calpoly.edu>
2026-04-29 12:58:04 -0700

    feat * first draft tasks class complete

---
+++ src/Tasks.java
@@ -0,0 +1,78 @@
+/*
+Author: Ivan T. 
+
+Random Ideas:
+- Need method to name task
+- Need method to assign user to task
+- Need method to assign value to task
+*/
+
+//imports
+import java.Util.Scanner;
+
+
+public class Tasks {
+
+    public Task(String taskName, String userAssignment, int taskValue)
+    {
+        /*
+        Method Description: constructor
+        */
+        this.taskName = taskName;
+        this.userAssignment = userAssignment; 
+        this.taskValue = taskValue;
+    }
+
+    
+    public void setTaskName()
+    {
+        /*
+        Method Description: assign a task object's taskName
+        */
+       Scanner input = new Scanner(System.in); // set up scanner object to get console info
+
+       System.out.println("Enter Task Name: ") // prompt user to enter task name 
+
+       String newTaskName = input.nextLine();  //read task name from console
+
+       this.taskName = newTaskName; //assign taskName
+
+    }
+
+    public void setUserAssignment()
+    {
+        /*
+        Method Description: assign a task user
+        */
+
+       Scanner input = new Scanner(System.in); // set up scanner object to get console info
+
+       System.out.println("Enter User Assignment: ") // prompt user to enter user assignment 
+
+       String newUserAssignment = input.nextLine();  //read user name from console
+
+       this.userAssignment = newUserAssignment; //assign taskName
+
+    }
+
+    public void setTaskValue()
+    {
+        /*
+        Method Description: assign a task object's taskName
+        */
+
+        Scanner input = new Scanner(System.in); // set up scanner object to get console info
+
+        System.out.println("Enter Task Value"); // prompt user to enter user value
+
+        int newTaskValue = input.nextInt(); // read from console
+
+        this.taskValue = newTaskValue(); // assign task
+
+
+    }
+
+    
+
+
+}
