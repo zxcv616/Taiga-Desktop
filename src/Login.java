b31783664be3bc5da00b559abaf13c5c1bff76f8
itorriani <itorrian@calpoly.edu>
2026-04-29 22:23:53 -0700

    feat * added login

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,73 @@
+/*
+
+*/
+
+//imports
+import java.util.LinkedList;
+import java.util.Scanner;
 
 
-public class Login { 
+public class Login {
+
+
+    //initialize variables
+
+    private String user; 
+    
+    private String pass; 
+
+
+
+
+    public Stories(); // empty constructor
+
+    
+    public void setUser()
+    {
+        /*
+        Description: Get user input to set subect line. Refer to tasks 
+        class for line by line comments
+        */
+
+       Scanner input = new Scanner(System.in);
+
+       System.out.println("Enter the Username: ");
+
+       String newUser = input.nextLine();
+
+       this.user = newUser;
+
+    }
+
+    public void setPass()
+    {
+        /*
+        Description: Get user input to set the description. Refer to tasks 
+        class for line by line comments
+        */
+
+       Scanner input = new Scanner(System.in);
+
+       System.out.println("Enter Password: ");
+
+       String newPass = input.nextLine();
+
+       this.pass = newPass;
+
+    }
+
+    public boolean checkValidity(String validUser, String validPass)
+    {
+
+        /*
+        Description: Check if the user and password is correct
+        based on some future knowledgeo f what the user/password is
+        */
+
+
+        if (!(user.equals(validUser) || !(pass.equals(validPass)) return false )); 
+
+    }
+
 
 }
